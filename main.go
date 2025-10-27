49b3494b8a6e18eaaacd1dac684b679d0817134e
eryan <ryan.elqali@ynov.com>
2025-10-27 15:08:55 +0100

    pu

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 	"net/http"
 )
 
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
+}
+
 func URLHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>URL visitée</h1><p>Vous avez visité : %s</p>", r.URL.Path)
 }
@@ -14,8 +18,9 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Site Web en Go</h1><p>Page d'accueil</p>")
 	})
-	
+
 	http.HandleFunc("/url", URLHandler)
+	http.HandleFunc("/color", ColorHandler)
 
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
